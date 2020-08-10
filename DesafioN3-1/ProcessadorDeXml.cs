33050261c2dde0dfcc17ca1764b993546cfe38bf
João Pedro Boell <joao.correa@hiper.com.br>
2020-08-10 17:43:37 -0300

    Add project files.

---
+++ DesafioN3-1/ProcessadorDeXml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DesafioN3_1
+{
+    public class ProcessadorDeXml
+    {
+        public List<XmlDocument> Xmls { get; set; }
+
+        public ProcessadorDeXml()
+        {
+            Xmls = new List<XmlDocument>();
+        }
+
+        public decimal retornaTotalXmls(string caminho)
+        {
+            carregaXmls(caminho);
+            return somaTotalXml();
+        }
+
+        public decimal somaTotalXml()
+        {
+            var total = (decimal)0;
+            Parallel.ForEach(Xmls, (x) =>
+            {
+                total += decimal.Parse(x.GetElementsByTagName("vNF")[0].InnerXml);
+            });
+            return total;
+        }
+
+        public void carregaXmls(string caminho)
+        {   
+            var xmlsNaPasta = new DirectoryInfo(caminho)
+                .GetFiles()
+                .OrderBy(x => x.Name)
+                .ToArray();
+            for (var i = 0; i < xmlsNaPasta.Length; i++)
+            {
+                var xml = new XmlDocument();
+                xml.Load(xmlsNaPasta[i].FullName);
+                Xmls.Add(xml);
+            }    
+        }
+    }
+}
